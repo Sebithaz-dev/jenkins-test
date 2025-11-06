4e4bb339f5365fb19efae3ec262dfcc639926010
Sebithaz <se.escobarn@duocuc.cl>
2025-11-06 17:07:19 -0300

    Update vulnerable_server.py

--- vulnerable_server.py
+++ vulnerable_server.py
@@ -2,10 +2,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Welcome to the vulnerable app!"
+
+
 @app.route('/hello', methods=['GET'])
 def hello():
     name = request.args.get('name')
     return f'Hello, {name}!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
